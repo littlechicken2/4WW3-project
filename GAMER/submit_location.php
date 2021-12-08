7f83362ec2e29064f41c185052c3eb6292182eb2
Pikappy <N_D58@hotmail.com>
2021-12-07 21:51:13 -0500

    Shifted php up

--- GAMER/submit_location.php
+++ GAMER/submit_location.php
@@ -1,121 +1,4 @@
-<!DOCTYPE HTML>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>GamerFriend</title>
-  <link rel="stylesheet" href="project.css">
-  <!-- Leaflet API -->
-  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-   integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-   crossorigin=""/>
-  <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
-   integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-   crossorigin=""></script>
-  <!-- Animation Library -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
-
-  <!-- Initiate Map Size and Styling for Footer - https://matthewjamestaylor.com/bottom-footer -->
-  <style>
-    #map{
-      height: 500px;
-      width: 100%;
-    }
-    html,body {
-       height:100%;
-    }
-    #container {
-       min-height:100%;
-       position:relative;
-    }
-    #footer {
-       position:absolute;
-       bottom:0;
-       width:100%;
-       height:60px;   /* Height of the footer */
-    }
-    input[type="text"]::placeholder {
-      color: grey;
-    }
-    input[type="number"]::placeholder {
-      color: grey;
-    }
-  </style>
-</head>
-<body>
-
-  <img src="10.png" class="bg" alt="bg">
-  <div id="container">
-    <!-- NavBar -->
-    <?php 
-      // Initialize the session
-      session_start();
-      require_once "config.php";
-      if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-        include 'notloggedin.inc';
-      }
-      else{
-        include 'loggedin.inc';
-      }
-    ?>
-
-    <!-- Header -->
-    <div class="width">
-      <h1 class = "neonText">NEW LOCATION</h1>
-      <hr class="neon">
-      <p></p>
-
-      <!-- Location Submission Form -->
-      <form class="modal-content animate" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <div class="neonbox container">
-          <p></p>
-
-          <label id="name"><b>Location Name: </b></label>
-          <input type="text" placeholder="Limeridge Mall" class="form-control <?php echo (!empty($inputname_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $inputname; ?>" 
-          id="inputname" required>
-          <span class="invalid-feedback"><?php echo $inputname_err; ?></span>
-          <p></p>
-
-          <label id="address"><b>Street Address: </b></label>
-          <input type="text" placeholder="999 Upper Wentworth" class="form-control <?php echo (!empty($inputaddress)) ? 'is-invalid' : ''; ?>" value="<?php echo $inputaddress; ?>" 
-          id="inputaddress" required>
-          <span class="invalid-feedback"><?php echo $inputaddress_err; ?></span>
-          <p></p>
-
-          <label id="city"><b>City: </b></label>
-          <input type="text" placeholder="Hamilton" class="form-control <?php echo (!empty($inputcity)) ? 'is-invalid' : ''; ?>" value="<?php echo $inputcity; ?>"
-          id="inputcity" required>
-          <span class="invalid-feedback"><?php echo $inputcity_err; ?></span>
-          <p></p>
-
-          <label id="province"><b>Province: </b></label>
-          <input type="text" placeholder="Ontario" class="form-control <?php echo (!empty($inputprovince)) ? 'is-invalid' : ''; ?>" value="<?php echo $inputprovince; ?>"
-          id="inputprovince" required>
-          <span class="invalid-feedback"><?php echo $inputprovince_err; ?></span>
-          <p></p>
-
-          <label id="postal code"><b>Postal Code: </b></label>
-          <input type="text" placeholder="L9A 4X5" class="form-control <?php echo (!empty($inputpostalcode)) ? 'is-invalid' : ''; ?>" value="<?php echo $inputpostalcode; ?>"
-          id="inputpostalcode" required>
-          <span class="invalid-feedback"><?php echo $inputpostalcode_err; ?></span>
-          <p></p>
-
-          <label id="phone"><b>Phone Number: </b></label>
-          <input type="text" placeholder="905-318-6089" class="form-control <?php echo (!empty($inputphone)) ? 'is-invalid' : ''; ?>" value="<?php echo $inputphone; ?>"
-          id="inputphone" required>
-          <span class="invalid-feedback"><?php echo $inputphone_err; ?></span>
-          <p></p>
-
-          <label id="coordinate"><b>(Latitude,Longtitude): </b></label>
-          <input type="number" placeholder="43.217627" class="form-control <?php echo (!empty($latitude)) ? 'is-invalid' : ''; ?>" value="<?php echo $latitude; ?>"
-          id="latitude" required min="-90" max="90" step="0.001">
-          <span class="invalid-feedback"><?php echo $latitude_err; ?></span>
-          
-          <input type="number" placeholder="-79.863722" class="form-control <?php echo (!empty($longitude)) ? 'is-invalid' : ''; ?>" value="<?php echo $longitude; ?>"
-          id="longitude" required min="-180" max="180" step="0.001">
-          <span class="invalid-feedback"><?php echo $longitude_err; ?></span>
-          <p></p>
-
-          <?php
+<?php
             require_once 'config.php';
 
             // Define variables and initialize with empty values
@@ -242,6 +125,125 @@
             // Close connection
             unset($pdo);
           ?>
+          
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>GamerFriend</title>
+  <link rel="stylesheet" href="project.css">
+  <!-- Leaflet API -->
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+   integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+   crossorigin=""/>
+  <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+   integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+   crossorigin=""></script>
+  <!-- Animation Library -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+
+  <!-- Initiate Map Size and Styling for Footer - https://matthewjamestaylor.com/bottom-footer -->
+  <style>
+    #map{
+      height: 500px;
+      width: 100%;
+    }
+    html,body {
+       height:100%;
+    }
+    #container {
+       min-height:100%;
+       position:relative;
+    }
+    #footer {
+       position:absolute;
+       bottom:0;
+       width:100%;
+       height:60px;   /* Height of the footer */
+    }
+    input[type="text"]::placeholder {
+      color: grey;
+    }
+    input[type="number"]::placeholder {
+      color: grey;
+    }
+  </style>
+</head>
+<body>
+
+  <img src="10.png" class="bg" alt="bg">
+  <div id="container">
+    <!-- NavBar -->
+    <?php 
+      // Initialize the session
+      session_start();
+      require_once "config.php";
+      if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+        include 'notloggedin.inc';
+      }
+      else{
+        include 'loggedin.inc';
+      }
+    ?>
+
+    <!-- Header -->
+    <div class="width">
+      <h1 class = "neonText">NEW LOCATION</h1>
+      <hr class="neon">
+      <p></p>
+
+      <!-- Location Submission Form -->
+      <form class="modal-content animate" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <div class="neonbox container">
+          <p></p>
+
+          <label id="name"><b>Location Name: </b></label>
+          <input type="text" placeholder="Limeridge Mall" class="form-control <?php echo (!empty($inputname_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $inputname; ?>" 
+          id="inputname" required>
+          <span class="invalid-feedback"><?php echo $inputname_err; ?></span>
+          <p></p>
+
+          <label id="address"><b>Street Address: </b></label>
+          <input type="text" placeholder="999 Upper Wentworth" class="form-control <?php echo (!empty($inputaddress)) ? 'is-invalid' : ''; ?>" value="<?php echo $inputaddress; ?>" 
+          id="inputaddress" required>
+          <span class="invalid-feedback"><?php echo $inputaddress_err; ?></span>
+          <p></p>
+
+          <label id="city"><b>City: </b></label>
+          <input type="text" placeholder="Hamilton" class="form-control <?php echo (!empty($inputcity)) ? 'is-invalid' : ''; ?>" value="<?php echo $inputcity; ?>"
+          id="inputcity" required>
+          <span class="invalid-feedback"><?php echo $inputcity_err; ?></span>
+          <p></p>
+
+          <label id="province"><b>Province: </b></label>
+          <input type="text" placeholder="Ontario" class="form-control <?php echo (!empty($inputprovince)) ? 'is-invalid' : ''; ?>" value="<?php echo $inputprovince; ?>"
+          id="inputprovince" required>
+          <span class="invalid-feedback"><?php echo $inputprovince_err; ?></span>
+          <p></p>
+
+          <label id="postal code"><b>Postal Code: </b></label>
+          <input type="text" placeholder="L9A 4X5" class="form-control <?php echo (!empty($inputpostalcode)) ? 'is-invalid' : ''; ?>" value="<?php echo $inputpostalcode; ?>"
+          id="inputpostalcode" required>
+          <span class="invalid-feedback"><?php echo $inputpostalcode_err; ?></span>
+          <p></p>
+
+          <label id="phone"><b>Phone Number: </b></label>
+          <input type="text" placeholder="905-318-6089" class="form-control <?php echo (!empty($inputphone)) ? 'is-invalid' : ''; ?>" value="<?php echo $inputphone; ?>"
+          id="inputphone" required>
+          <span class="invalid-feedback"><?php echo $inputphone_err; ?></span>
+          <p></p>
+
+          <label id="coordinate"><b>(Latitude,Longtitude): </b></label>
+          <input type="number" placeholder="43.217627" class="form-control <?php echo (!empty($latitude)) ? 'is-invalid' : ''; ?>" value="<?php echo $latitude; ?>"
+          id="latitude" required min="-90" max="90" step="0.001">
+          <span class="invalid-feedback"><?php echo $latitude_err; ?></span>
+          
+          <input type="number" placeholder="-79.863722" class="form-control <?php echo (!empty($longitude)) ? 'is-invalid' : ''; ?>" value="<?php echo $longitude; ?>"
+          id="longitude" required min="-180" max="180" step="0.001">
+          <span class="invalid-feedback"><?php echo $longitude_err; ?></span>
+          <p></p>
+
+          
 
           <button id = "show" onclick="showPosition2()"> Show </button>
           <p></p>
